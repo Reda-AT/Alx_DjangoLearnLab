5816804da79e216d8843178209c991778ce770ef
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-01 19:39:50 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import list_books
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 
