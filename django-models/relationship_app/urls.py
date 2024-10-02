48ab0e5e03fdae3e6f47ee94c422a73cfde5310b
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-02 10:07:31 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 from .views import list_books
 urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 
     # Route pour le logout (utilise la vue intégrée de Django)
