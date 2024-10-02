65200d0d99021ac6113d77d7f42e2c74d8f8442a
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-02 09:58:23 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import user_passes_test
 
+# Check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 # Function-Based View (FBV) pour lister tous les livres
 def list_books(request):
     books = Book.objects.all()
