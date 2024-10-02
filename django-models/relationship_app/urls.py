d0175db8233f1a085a5c80f41f236b77aeff1758
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-02 12:08:25 +0100

    Add files via upload

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+from .views import list_books
+urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Route pour le logout (utilise la vue intégrée de Django)
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Route pour l'inscription
+    path('register/', views.register, name='register'),
+    # Function-based view for listing all books
+    path('books/', views.list_books, name='list_books'),
+
+    # Class-based view for library details
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import user_passes_test
+
+# Check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+# Function-Based View (FBV) pour lister tous les livres
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+# Class-Based View (CBV) pour afficher les détails d'une bibliothèque
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('list_books')  # Redirect to any view after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
