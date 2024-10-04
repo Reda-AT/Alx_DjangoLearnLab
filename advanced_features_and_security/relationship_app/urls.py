598766c3a0e500ff766c29fca78fbb1d8330f1fb
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-04 16:20:11 +0100

    Add files via upload

---
+++ advanced_features_and_security/relationship_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Route pour le logout (utilise la vue intégrée de Django)
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Route pour l'inscription
+    path('register/', views.register, name='register'),
+    # Function-based view for listing all books
+    path('books/', views.list_books, name='list_books'),
+
+    # Class-based view for library details
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
