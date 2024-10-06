6de3ad430a90d717fff9be46cc0a2180a537b13a
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-10-06 12:50:58 +0100

    Add files via upload

---
+++ django-models/LibraryProject/Scripts/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+
+# Register your models here.
+from .models import Book
+
+class BookAdmin(admin. ModelAdmin):
+   list_filter = ('title', 'author', 'publication_year')
+   search_fields = ('title','author')
+   
+# Model Register
+
+
+
